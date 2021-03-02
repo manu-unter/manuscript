68718775b60c9c9fc86aa93395da7d4829b8f199
Manuel Unterhofer <manuel.unterhofer@hey.com>
2021-03-02 19:04:10 +0100

    Reintroduce app icons and webmanifest

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,27 @@ export default class CustomDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e29e1a" />
+          <meta name="msapplication-TileColor" content="#00aba9" />
+          <meta name="theme-color" content="#e29e1a" />
           <TypographyStyle typography={typography} />
           <GoogleFont typography={typography} />
         </Head>
